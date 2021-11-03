bcb0ba25c87d49b3d8451bec73a9923a7f22deb6
Sabrina <sah.luciana@gmail.com>
2021-11-02 21:49:29 -0300

    :construction: Create Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-
-import styled from 'styled-components'
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 
-const Title = styled.h1`
-font-size: 64px;
-color: #8257e6;
-`
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <Title>Hello Budd</Title>
+      <Header />
       
-    </div>
+      
+    </>
   );
 }
 
