7bcf7b968bd1589a413d264f2e6cf18c935bfa6f
Sabrina <sah.luciana@gmail.com>
2021-11-07 14:34:51 -0300

    :construction: checkpoint: Entrada Saida unica state

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,17 @@
 
 import Modal from 'react-modal';
-import { TransactionModalContainer, TransactionTypeContainer } from './styles';
+import { TransactionModalContainer, TransactionTypeContainer, RadioBoxButton } from './styles';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react';
 interface INewTransactionModal {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: INewTransactionModal){
-    
+    const [ typeTransactions, setTypeTransactions] = useState('deposit');
 
     return(
         <Modal
@@ -27,32 +28,45 @@ export function NewTransactionModal({isOpen, onRequestClose}: INewTransactionMod
             </button>
             <h2>Cadastrar Transação</h2>
             <TransactionModalContainer>
-            <input 
-            type="text" 
-            placeholder="Título"
-            />
-            <input 
-            type="number"
-            placeholder="Valor" 
-            />
-            
-            <TransactionTypeContainer>
-                <button>
-                <span>Entrada</span>
-                <img src={outcomeImg} alt="Entrada" />
-                </button>
-                <button>
-                <img src={outcomeImg} alt="Saída" />
-                <span>Saída</span>
-                </button>
-            </TransactionTypeContainer>
+                <input 
+                type="text" 
+                placeholder="Título"
+                />
+                <input 
+                type="number"
+                placeholder="Valor" 
+                />
+                
+                <TransactionTypeContainer>
 
-            <input 
-            placeholder="Categoria"
-            />
-            <button type="submit">
-                Cadastrar
-            </button>
+                    <RadioBoxButton 
+                    type="button"
+                    onClick={()=> {setTypeTransactions('deposit');}}
+                    isActive={ typeTransactions === 'deposit'}
+                    // className={type === 'deposit'?? 'active' : ''}
+                    >
+                        <span>Entrada</span>
+                        <img src={incomeImg} alt="Entrada" 
+                        />
+                    </RadioBoxButton>
+
+                    <RadioBoxButton 
+                    type="button"
+                    onClick={()=> {setTypeTransactions('withdraw');}}
+                    isActive={ typeTransactions === 'withdraw'}
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBoxButton>
+
+                </TransactionTypeContainer>
+
+                <input 
+                placeholder="Categoria"
+                />
+                <button type="submit">
+                    Cadastrar
+                </button>
             </TransactionModalContainer>
         </Modal>
     )
